2de70be69a1d41fc5a67b68aa791104203fb52e9
zephyrus21 <pandey.piyus@gmail.com>
2022-11-01 15:46:24 +0530

    Initial Commit

---
+++ grocios/grociosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grociosApp.swift
+//  grocios
+//
+//  Created by Piyush Pandey on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct grociosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
